7d1541035c86e2b90cebfc481287ec0a0f42cace
John Conley <john.conley@nordicsemi.no>
2023-07-24 11:21:51 -0700

    breaking: use new ts patch format

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const {di} = require("@jfconley/di-compiler");
 
-const transformer = (program) => di({ program })
+
+const transfomerCache = new Map();
+const transformer = (program, config) => {
+    if (!transfomerCache.has(program)) {
+        transfomerCache.set(program, di({program}));
+    }
+    const transformer = transfomerCache.get(program);
+
+    if (config.after) {
+        return transformer.after[0] || (() => {})
+    } else {
+        return transformer.before[0]
+    }
+}
 
 exports.default = transformer;
