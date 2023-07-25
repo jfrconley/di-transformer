e9fc6540ae22d8fde9ae2050b8adcb84eb0e3276
John Conley <john.conley@nordicsemi.no>
2023-07-24 18:01:40 -0700

    fix import preservation

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const {di} = require("@jfconley/di-compiler");
 
 
 const transfomerCache = new Map();
-const transformer = (program, config) => {
+const transformer = (after, program) => {
+    console.log(after)
     if (!transfomerCache.has(program)) {
         transfomerCache.set(program, di({program}));
     }
     const transformer = transfomerCache.get(program);
 
-    if (config.after) {
+    if (after) {
         return transformer.after[0] || (() => {})
     } else {
         return transformer.before[0]
     }
 }
 
-exports.default = transformer;
+
+
+exports.default = (program) => transformer(false, program)
+exports.preserve = (program) => transformer(true, program)
